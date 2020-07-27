c1e6b623df39946286619d838d5cf1af6a7ac143
Nilesh Naseet <nileshnaseet@gmail.com>
2020-07-27 00:22:10 -0400

    Initial Version

---
+++ MyVidly/Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using MyVidly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyVidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie { Name = "Baahubali!" };
+
+            return View(movie);
+        }
+
+        [Route("movies/released/{year}/{month}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        { 
+            return Content(String.Format("Year={0} & Month = {1}",year,month));
+        }
+    }
+}
